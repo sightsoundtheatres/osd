6fb567b9137040b87a8639d633f47fddbbfed915
Matthew Miles <matthew.miles@sight-sound.com>
2024-01-08 16:03:06 -0600

    Testing cleanup

--- functions/oobeFunctions.ps1
+++ functions/oobeFunctions.ps1
@@ -388,10 +388,10 @@ function Step-oobeRestartComputer {
         # Removing downloaded content
         Write-Host -ForegroundColor Yellow "[-] Cleaning up... Removing temperary directories"
         # Get-ChildItem -Path "C:\osdcloud\*" | Where-Object { $_.Name -ne "Logs" } | ForEach-Object { Remove-Item $_.FullName -Force -Recurse }
-        if (Test-Path "C:\osdcloud" -PathType Container) { Remove-Item -LiteralPath "C:\osdcloud" -Force -Recurse }
-        if (Test-Path "C:\Drivers" -PathType Container) { Remove-Item -LiteralPath "C:\osdcloud" -Force -Recurse }
-        if (Test-Path "C:\Dell" -PathType Container) { Remove-Item -LiteralPath "C:\osdcloud" -Force -Recurse }
-        if (Test-Path "C:\Temp" -PathType Container) { Remove-Item -LiteralPath "C:\osdcloud" -Force -Recurse }
+        if (Test-Path "C:\osdcloud" -PathType Container) { Remove-Item "C:\osdcloud" -Force -Recurse }
+        if (Test-Path "C:\Drivers" -PathType Container) { Remove-Item "C:\osdcloud" -Force -Recurse }
+        if (Test-Path "C:\Dell" -PathType Container) { Remove-Item "C:\osdcloud" -Force -Recurse }
+        if (Test-Path "C:\Temp" -PathType Container) { Remove-Item "C:\osdcloud" -Force -Recurse }
         Write-Host -ForegroundColor Green '[+] Build Complete!'
         Write-Warning 'Device will restart in 30 seconds.  Press Ctrl + C to cancel'
         Stop-Transcript
