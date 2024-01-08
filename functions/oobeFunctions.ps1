89801b43fc429ccedd2e26b5e4bc42f6fcf393e6
Matthew Miles <matthew.miles@sight-sound.com>
2024-01-08 12:56:06 -0600

    24.1.8.7

--- functions/oobeFunctions.ps1
+++ functions/oobeFunctions.ps1
@@ -431,8 +431,7 @@ if (Test-Path -Path 'C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe') {
 
 function Step-oobeSetDateTime {
     [CmdletBinding()]
-    param ()
-    if (($env:UserName -eq 'defaultuser0') -and ($Global:oobeCloud.oobeSetDateTime -eq $true)) {
+    param ()    
         Write-Host -ForegroundColor Yellow 'Verify the Date and Time is set properly including the Time Zone'
         Write-Host -ForegroundColor Yellow 'If this is not configured properly, Certificates and Autopilot may fail'
         Start-Process 'ms-settings:dateandtime' | Out-Null
@@ -441,6 +440,6 @@ function Step-oobeSetDateTime {
             Wait-Process $ProcessId
         }
     }
-}
+
 
 
