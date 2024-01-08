26a9f14d8cb8142dad7c51f60187248b75c3ab8b
Matthew Miles <matthew.miles@sight-sound.com>
2024-01-08 11:34:20 -0600

    Adding service check

--- modules/devicesdell.psm1
+++ modules/devicesdell.psm1
@@ -198,6 +198,9 @@ function osdcloud-DCUAutoUpdate {
     <#
     Enables DCU Auto Update
     #>
+    $DCUServiceName = 'DellClientManagement'
+
+    # Check if the DCU CLI executable exists
     if (Test-Path -Path 'C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe') {
         $ProcessPath = 'C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe'
         Write-Host -ForegroundColor Green "[+] Dell Command Update installed"
@@ -206,7 +209,17 @@ function osdcloud-DCUAutoUpdate {
         Write-Host -ForegroundColor Green "[+] Dell Command Update installed"
     } else {
         Write-Host -ForegroundColor Cyan "[-] System not = Dell - DCU not supported"
+        return  # Stop script execution if DCU is not supported
     }
+
+    # Check if the Dell Client Management Service is running
+    $DCUService = Get-Service -Name $DCUServiceName -ErrorAction SilentlyContinue
+
+    if ($DCUService -ne $null -and $DCUService.Status -eq 'Running') {
+        Write-Host -ForegroundColor Yellow "[-] Stopping Dell Client Management Service..."
+        Stop-Service -Name $DCUServiceName
+        Start-Sleep -Seconds 5  # Give some time for the service to stop
+    }    
     
     $ProcessArgs = "/configure -scheduleAuto -scheduleAction=DownloadInstallAndNotify"
     $DCU = Start-Process -FilePath $ProcessPath -ArgumentList $ProcessArgs -Wait -PassThru -NoNewWindow
