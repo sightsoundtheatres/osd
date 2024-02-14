c939c725fafb3aa17e1ad4b6f0f2f6cab50fd375
Matthew Miles <matthew.miles@sight-sound.com>
2024-02-14 10:33:40 -0600

    Autopilot testing

--- functions/oobeFunctions.ps1
+++ functions/oobeFunctions.ps1
@@ -174,6 +174,50 @@ function Step-oobeInstallModuleAutopilotOOBE {
         }
     }
 }
+
+function Step-oobeInstallModuleGetWindowsAutopilotInfoCommunity {
+    [CmdletBinding()]
+    param ()
+        # Install the get-windowsautopilotcommunity.ps1 script
+        install-script get-windowsautopilotinfocommunity -Force
+
+        # Define the options for the GroupTag parameter
+        $GroupTagOptions = @("Development", "Enterprise")
+
+        # Display the menu for the GroupTag parameter
+        Write-Host "Select a GroupTag:"
+        for ($i = 0; $i -lt $GroupTagOptions.Count; $i++) {
+            Write-Host "$($i + 1): $($GroupTagOptions[$i])"
+        }
+        $GroupTagChoice = Read-Host "Enter your choice"
+        $GroupTag = $GroupTagOptions[$GroupTagChoice - 1]
+
+        # Prompt the user to enter a value for the AssignedComputerName parameter
+        do {
+            $AssignedComputerName = Read-Host "Enter the AssignedComputerName ex XXWIN-EID-XXXX (15 characters or less)"
+            if ($AssignedComputerName.Length -gt 15) {
+                Write-Warning "AssignedComputerName must be 15 characters or less"
+            }
+        } while ($AssignedComputerName.Length -gt 15)
+
+        # Define the options for the AddToGroup parameter
+        $AddToGroupOptions = @( "Autopilot_Devices-GeneralUsers",
+                                "Autopilot_Devices-Box_CC",
+                                "AutoPilot_Devices-Retail",
+                                "Autopilot_Devices-CenterStageKiosk",
+                                "Autopilot_Devices-SharedDevice_IT")
+
+        # Display the menu for the AddToGroup parameter
+        Write-Host "Select an AddToGroup option:"
+        for ($i = 0; $i -lt $AddToGroupOptions.Count; $i++) {
+            Write-Host "$($i + 1): $($AddToGroupOptions[$i])"
+        }
+        $AddToGroupChoice = Read-Host "Enter your choice"
+        $AddToGroup = $AddToGroupOptions[$AddToGroupChoice - 1]
+
+        # Call the get-windowsautopilotinfo.ps1 script with the specified parameters
+        get-windowsautopilotinfocommunity.ps1 -online -GroupTag $GroupTag -AssignedComputerName $AssignedComputerName -AddToGroup $AddToGroup
+}
 function Step-oobeRegisterAutopilot {
     [CmdletBinding()]
     param (
@@ -188,7 +232,8 @@ function Step-oobeRegisterAutopilot {
         
         if ($result -eq [System.Windows.Forms.DialogResult]::Yes) {
             Write-Host -ForegroundColor Yellow "[-] Registering Device in Autopilot using AutopilotOOBE"
-            Step-oobeInstallModuleAutopilotOOBE
+            #Step-oobeInstallModuleAutopilotOOBE
+            Step-oobeInstallModuleGetWindowsAutopilotInfoCommunity
         }
         else {
             Write-Host -ForegroundColor Cyan "[!] Device registration with Autopilot skipped."
