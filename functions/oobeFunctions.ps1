8a31e2a7a4bb738b69c4fe568ceeb6453a5cca6c
Matthew Miles <matthew@miles-family.com>
2024-02-15 09:37:39 -0600

    Trying community getwindowsautopilotinfo

--- functions/oobeFunctions.ps1
+++ functions/oobeFunctions.ps1
@@ -216,7 +216,7 @@ function Step-oobeInstallModuleGetWindowsAutopilotInfoCommunity {
         $AddToGroup = $AddToGroupOptions[$AddToGroupChoice - 1]
 
         # Call the get-windowsautopilotinfo.ps1 script with the specified parameters
-        get-windowsautopilotinfocommunity.ps1 -GroupTag $GroupTag -AssignedComputerName $AssignedComputerName -AddToGroup $AddToGroup -online
+        get-windowsautopilotinfocommunity.ps1 -Assign -GroupTag $GroupTag -AssignedComputerName $AssignedComputerName -AddToGroup $AddToGroup -online
 }
 function Step-oobeRegisterAutopilot {
     [CmdletBinding()]
@@ -232,8 +232,8 @@ function Step-oobeRegisterAutopilot {
         
         if ($result -eq [System.Windows.Forms.DialogResult]::Yes) {
             Write-Host -ForegroundColor Yellow "[-] Registering Device in Autopilot using AutopilotOOBE"
-            Step-oobeInstallModuleAutopilotOOBE
-            #Step-oobeInstallModuleGetWindowsAutopilotInfoCommunity
+            #Step-oobeInstallModuleAutopilotOOBE
+            Step-oobeInstallModuleGetWindowsAutopilotInfoCommunity
         }
         else {
             Write-Host -ForegroundColor Cyan "[!] Device registration with Autopilot skipped."
