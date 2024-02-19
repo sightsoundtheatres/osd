31df7dca1c5265124203bac59ad7974f41bae3ff
Matthew Miles <matthew@miles-family.com>
2024-02-19 17:29:04 -0600

    Comment out shutdown schedule

--- cloudscripts/POS/Setup-POS.ps1
+++ cloudscripts/POS/Setup-POS.ps1
@@ -232,11 +232,11 @@ Write-Host -ForegroundColor Cyan "[!] Install notes .txt file saved in c:\temp"
 # Create shutdown schedule
 ########################################################
 
-$Action = New-ScheduledTaskAction -Execute 'shutdown.exe' -Argument '/s /f /t 0'
-$DaysOfWeek = [System.DayOfWeek]::Sunday, [System.DayOfWeek]::Monday, [System.DayOfWeek]::Tuesday, [System.DayOfWeek]::Thursday, [System.DayOfWeek]::Friday, [System.DayOfWeek]::Saturday
-$Trigger = New-ScheduledTaskTrigger -DaysOfWeek $DaysOfWeek -At 12:00am
-Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName "Shutdown" -Description "Shutdown computer every night at midnight, except for Wednesday night"
-Write-Host -ForegroundColor Green "[+] Shutdown scheduled task created"
+#$Action = New-ScheduledTaskAction -Execute 'shutdown.exe' -Argument '/s /f /t 0'
+#$DaysOfWeek = [System.DayOfWeek]::Sunday, [System.DayOfWeek]::Monday, [System.DayOfWeek]::Tuesday, [System.DayOfWeek]::Thursday, [System.DayOfWeek]::Friday, [System.DayOfWeek]::Saturday
+#$Trigger = New-ScheduledTaskTrigger -DaysOfWeek $DaysOfWeek -At 12:00am
+#Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName "Shutdown" -Description "Shutdown computer every night at midnight, except for Wednesday night"
+#Write-Host -ForegroundColor Green "[+] Shutdown scheduled task created"
 
 
 # Restart computer
