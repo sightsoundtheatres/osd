1e0071bb15a29b434d427ef96acfa22ba69e38cb
Matthew Miles <matthew.miles@sight-sound.com>
2024-05-27 12:35:15 -0500

    Removed uninstall command

--- cloudscripts/POS/reinstall-POS.ps1
+++ cloudscripts/POS/reinstall-POS.ps1
@@ -1,19 +1,7 @@
 # Uninstall StoreCommerce app
 ########################################################
 
-# Get the package information for the Microsoft Store Commerce application for all users
-$packages = Get-AppxPackage -AllUsers -Name "Microsoft.StorePurchaseApp"
-
-# Check if any packages were found
-if ($packages) {
-    foreach ($package in $packages) {
-        # Uninstall the Microsoft Store Commerce application for all users
-        Remove-AppxPackage -Package $package.PackageFullName -AllUsers
-        Write-Output "Uninstalled: $($package.Name) with PackageFullName: $($package.PackageFullName) for all users."
-    }
-} else {
-    Write-Output "Microsoft Store Commerce application is not installed for any users on this system."
-}
+#  Remove app from programs list manually
 
 
 # Download and install the StoreCommerce app 
