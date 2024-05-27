eceff6eb719fa720d0aa5f4c0fafb880cf9b5861
Matthew Miles <matthew.miles@sight-sound.com>
2024-05-27 11:49:02 -0500

    added reinstall-pos.ps1

---
+++ cloudscripts/POS/reinstall-POS.ps1
@@ -0,0 +1,35 @@
+# Uninstall StoreCommerce app
+########################################################
+
+# Get the package information for the Microsoft Store Commerce application
+$package = Get-AppxPackage -Name "Microsoft.StorePurchaseApp"
+
+# Check if the package is installed
+if ($package) {
+    # Uninstall the Microsoft Store Commerce application
+    Remove-AppxPackage -Package $package.PackageFullName
+    Write-Output -ForegroundColor Green "[+] Microsoft Store Commerce application has been uninstalled successfully."
+} else {
+    Write-Warning "Microsoft Store Commerce application is not installed on this system."
+}
+
+
+# Download and install the StoreCommerce app 
+########################################################
+
+$url = "https://ssintunedata.blob.core.windows.net/d365/StoreCommerce.Installer.exe"
+$outputDir = "C:\temp"
+$outputFile = Join-Path $outputDir "StoreCommerce.Installer.exe"
+
+# Check if the output directory exists and create it if necessary
+if (!(Test-Path $outputDir)) {
+    New-Item -ItemType Directory -Path $outputDir | Out-Null
+}
+
+# Download the file
+Invoke-WebRequest -Uri $url -OutFile $outputFile
+
+# Run the installer with the provided arguments
+cd $outputDir
+.\StoreCommerce.Installer.exe install --useremoteappcontent --retailserverurl "https://sst-prodret.operations.dynamics.com/Commerce"
+
