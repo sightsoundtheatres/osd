b3c579a49dee41d458c596d373b7e92c5042ccdf
Matthew Miles <67563275+1eyeITguy@users.noreply.github.com>
2024-07-28 17:43:57 -0500

    renaming files instead of deleting

--- functions/set-lockScreen_Wallpaper.ps1
+++ functions/set-lockScreen_Wallpaper.ps1
@@ -3,36 +3,13 @@ $WallpaperFolder = "C:\Windows\Web\Wallpaper\Windows"
 $LockScreenFolder = "C:\Windows\Web\Screen"
 $Wallpaper4KFolder = "C:\Windows\Web\4K\Wallpaper\Windows"
 
-# Define files
-$FilesToDelete = @(
+# Define files to rename
+$FilesToRename = @(
     "$WallpaperFolder\img0.jpg",
     "$LockScreenFolder\img100.jpg",
     "$LockScreenFolder\img105.jpg",
     "$Wallpaper4KFolder\img0_1920x1200.jpg"
 )
-
-# Define the principals
-$LocalAdministratorsPrincipal = "BUILTIN\Administrators"
-$TrustedInstallerPrincipal = "NT SERVICE\TrustedInstaller"
-
-# Function to take ownership of a folder
-function Take-Ownership {
-    param (
-        [parameter(Mandatory = $true)]
-        [string]$FolderPath
-    )
-
-    try {
-        # Take ownership of the folder
-        takeown /F $FolderPath /A /R /D Y | Out-Null
-
-        # Set the owner to TrustedInstaller
-        icacls $FolderPath /setowner "$($TrustedInstallerPrincipal)" /T /C /Q | Out-Null
-    }
-    catch {
-        # Suppress error output
-    }
-}
 
 # Function to set full control permissions for administrators group
 function Set-FullControlPermissions {
@@ -57,12 +34,12 @@ function Set-FullControlPermissions {
         Set-Acl $FilePath $acl | Out-Null
     }
     catch {
-        # Suppress error output
+        Write-Host "Failed to set permissions for $FilePath. Error: $_" -ForegroundColor Red
     }
 }
 
-# Function to delete files
-function Delete-Files {
+# Function to rename files
+function Rename-Files {
     param (
         [parameter(Mandatory = $true)]
         [string[]]$FilePaths
@@ -70,35 +47,49 @@ function Delete-Files {
 
     foreach ($FilePath in $FilePaths) {
         try {
-            # Delete the file
-            Remove-Item $FilePath -Force -ErrorAction Stop | Out-Null
+            $NewFilePath = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($FilePath), [System.IO.Path]::GetFileNameWithoutExtension($FilePath) + "_original" + [System.IO.Path]::GetExtension($FilePath))
+            Rename-Item -Path $FilePath -NewName $NewFilePath -Force -ErrorAction Stop
+            Write-Host "Renamed file: $FilePath to $NewFilePath" -ForegroundColor Green
         }
         catch {
-            # Suppress error output
+            Write-Host "Failed to rename file: $FilePath. Error: $_" -ForegroundColor Red
         }
     }
 }
 
-# Take ownership of each folder
-# Take-Ownership -FolderPath $WallpaperFolder | Out-Null
-# Take-Ownership -FolderPath $LockScreenFolder | Out-Null
-# Take-Ownership -FolderPath $Wallpaper4KFolder | Out-Null
+# Function to download files
+function Download-Files {
+    param (
+        [parameter(Mandatory = $true)]
+        [string]$Uri,
+        [parameter(Mandatory = $true)]
+        [string]$OutFile
+    )
+
+    try {
+        Invoke-WebRequest -UseBasicParsing -Uri $Uri -OutFile $OutFile -ErrorAction Stop | Out-Null
+        Write-Host "Downloaded file: $OutFile" -ForegroundColor Green
+    }
+    catch {
+        Write-Host "Failed to download file from $Uri to $OutFile. Error: $_" -ForegroundColor Red
+    }
+}
 
 # Set full control permissions for administrators group on each file
 $AdministratorsGroup = "BUILTIN\Administrators"
-Set-FullControlPermissions -FilePath $FilesToDelete[0] -Principal $AdministratorsGroup | Out-Null
-Set-FullControlPermissions -FilePath $FilesToDelete[1] -Principal $AdministratorsGroup | Out-Null
-Set-FullControlPermissions -FilePath $FilesToDelete[2] -Principal $AdministratorsGroup | Out-Null
-Set-FullControlPermissions -FilePath $FilesToDelete[3] -Principal $AdministratorsGroup | Out-Null
+foreach ($File in $FilesToRename) {
+    Set-FullControlPermissions -FilePath $File -Principal $AdministratorsGroup
+}
 
-# Delete the files
-Delete-Files -FilePaths $FilesToDelete | Out-Null
+# Rename the files
+Rename-Files -FilePaths $FilesToRename
 
-# Download and replace wallpaper and lock screen files
+# URLs for downloading new files
 $WallPaperURL = "https://ssintunedata.blob.core.windows.net/customization/img0_3840x2160.jpg"
 $LockScreenURL = "https://ssintunedata.blob.core.windows.net/customization/img100.jpg"
 
-Invoke-WebRequest -UseBasicParsing -Uri $WallPaperURL -OutFile "$WallpaperFolder\img0.jpg" | Out-Null
-Invoke-WebRequest -UseBasicParsing -Uri $LockScreenURL -OutFile "$LockScreenFolder\img100.jpg" | Out-Null
-Invoke-WebRequest -UseBasicParsing -Uri $LockScreenURL -OutFile "$LockScreenFolder\img105.jpg" | Out-Null
-Invoke-WebRequest -UseBasicParsing -Uri $WallPaperURL -OutFile "$Wallpaper4KFolder\img0_1920x1200.jpg" | Out-Null
+# Download and replace wallpaper and lock screen files
+Download-Files -Uri $WallPaperURL -OutFile "$WallpaperFolder\img0.jpg"
+Download-Files -Uri $LockScreenURL -OutFile "$LockScreenFolder\img100.jpg"
+Download-Files -Uri $LockScreenURL -OutFile "$LockScreenFolder\img105.jpg"
+Download-Files -Uri $WallPaperURL -OutFile "$Wallpaper4KFolder\img0_1920x1200.jpg"
