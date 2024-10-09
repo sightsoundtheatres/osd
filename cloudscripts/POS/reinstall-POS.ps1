a29e139c05d2cc54c27358addfa64eeb155494a0
Matthew Miles <matthew.miles@sight-sound.com>
2024-10-09 13:50:12 -0500

    Updated to use curl.exe

--- cloudscripts/POS/reinstall-POS.ps1
+++ cloudscripts/POS/reinstall-POS.ps1
@@ -5,6 +5,27 @@
 ############################################
 
 
+# Check Curl version and install if necessary
+function Install-Curl {
+    [CmdletBinding()]
+    param ()
+    if (-not (Get-Command 'curl.exe' -ErrorAction SilentlyContinue)) {
+        Write-Host -ForegroundColor Yellow "[-] Install Curl for Windows"
+        $Uri = 'https://curl.se/windows/latest.cgi?p=win64-mingw.zip'
+        Invoke-WebRequest -UseBasicParsing -Uri $Uri -OutFile "$env:TEMP\curl.zip"
+    
+        $null = New-Item -Path "$env:TEMP\Curl" -ItemType Directory -Force
+        Expand-Archive -Path "$env:TEMP\curl.zip" -DestinationPath "$env:TEMP\curl"
+    
+        Get-ChildItem "$env:TEMP\curl" -Include 'curl.exe' -Recurse | foreach {Copy-Item $_ -Destination "$env:SystemRoot\System32\curl.exe"}
+    }
+    else {
+        $GetItemCurl = Get-Item -Path "$env:SystemRoot\System32\curl.exe" -ErrorAction SilentlyContinue
+        Write-Host -ForegroundColor Green "[+] Curl $($GetItemCurl.VersionInfo.FileVersion)"
+    }
+}
+
+
 # Download and install the StoreCommerce app 
 ########################################################
 
@@ -17,8 +38,9 @@ if (!(Test-Path $outputDir)) {
     New-Item -ItemType Directory -Path $outputDir | Out-Null
 }
 
-# Download the file using Start-BitsTransfer
-Start-BitsTransfer -Source $url -Destination $outputFile -Priority Foreground
+# Download the file using curl
+Write-host -ForegroundColor -yellow "[!] Downloading StoreCommerce.Installer.exe"
+curl.exe -o $outputFile $url
 
 # Run the installer with the provided arguments
 cd $outputDir
