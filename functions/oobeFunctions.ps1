610445a758ad5c44c7bed1a7522f672014aa94e0
Matthew Miles <matthew.miles@sight-sound.com>
2024-12-20 15:27:40 -0600

    24.12.20.1

--- functions/oobeFunctions.ps1
+++ functions/oobeFunctions.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param()
 $ScriptName = 'oobeFunctions.sight-sound.dev'
-$ScriptVersion = '24.12.8.6'
+$ScriptVersion = '24.12.20.1'
 
 #region Initialize
 if ($env:SystemDrive -eq 'X:') {
@@ -667,8 +667,35 @@ function step-InstallModuleWinget {
 function step-setTimeZoneFromIP {
     [CmdletBinding()]
     param ()
-    Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/sightsoundtheatres/osd/refs/heads/main/functions/set-timeZoneFromIP_new.ps1)
-    Set-TimeZoneFromIP
+    # Fetch the time zone data from the API
+    $URIRequest = "http://worldtimeapi.org/api/ip"
+    $Response = (Invoke-WebRequest -Uri $URIRequest -UseBasicParsing).Content | ConvertFrom-Json
+
+    # Extract the timezone from the response
+    $TimeZoneAPI = $Response.timezone
+
+    # Define the mapping
+    $WindowsTimeZones = @{
+        "America/Chicago" = "Central Standard Time"
+        "America/New_York" = "Eastern Standard Time"
+        "America/Denver" = "Mountain Standard Time"
+        "America/Phoenix" = "US Mountain Standard Time" 
+        "America/Los_Angeles" = "Pacific Standard Time"
+        "America/Indiana/Indianapolis" = "Eastern Standard Time"
+        "America/Detroit" = "Eastern Standard Time"
+        "America/Kentucky/Louisville" = "Eastern Standard Time"
+        "America/North_Dakota/Center" = "Central Standard Time"
+        "America/Boise" = "Mountain Standard Time"
+    }
+
+    # Check if the timezone exists in the mapping
+    if ($WindowsTimeZones.ContainsKey($TimeZoneAPI)) {
+        $WindowsTimeZone = $WindowsTimeZones[$TimeZoneAPI]
+        Set-TimeZone -Id $WindowsTimeZone
+        Write-Host "[+] Time zone has been updated to - $WindowsTimeZone" -ForegroundColor Green 
+    } else {
+        Write-Warning "Time zone not found in the mapping. Using the default Windows time zone."
+    }
 }
 
 
