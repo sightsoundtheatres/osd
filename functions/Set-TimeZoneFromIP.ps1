057d796828ab7946717b5ca8715b50ba21bd3d2e
Matthew Miles <matthew.miles@sight-sound.com>
2024-12-20 15:15:05 -0600

    Adding new set-timezone script

---
+++ functions/Set-TimeZoneFromIP.ps1
@@ -0,0 +1,33 @@
+# Fetch the time zone data from the API
+$URIRequest = "http://worldtimeapi.org/api/ip"
+$Response = (Invoke-WebRequest -Uri $URIRequest -UseBasicParsing).Content | ConvertFrom-Json
+
+# Extract the timezone from the response
+$TimeZoneAPI = $Response.timezone
+
+# Define the mapping
+$WindowsTimeZones = @{
+    "America/Chicago" = "Central Standard Time"
+    "America/New_York" = "Eastern Standard Time"
+    "America/Denver" = "Mountain Standard Time"
+    "America/Phoenix" = "US Mountain Standard Time" 
+    "America/Los_Angeles" = "Pacific Standard Time"
+    "America/Indiana/Indianapolis" = "Eastern Standard Time"
+    "America/Detroit" = "Eastern Standard Time"
+    "America/Kentucky/Louisville" = "Eastern Standard Time"
+    "America/North_Dakota/Center" = "Central Standard Time"
+    "America/Boise" = "Mountain Standard Time"
+}
+
+# Check if the timezone exists in the mapping
+if ($WindowsTimeZones.ContainsKey($TimeZoneAPI)) {
+    $WindowsTimeZone = $WindowsTimeZones[$TimeZoneAPI]
+} else {
+    Write-Warning "Time zone not found in the mapping. Defaulting to Eastern Standard Time."
+    $WindowsTimeZone = "Eastern Standard Time"
+}
+
+# Set the system time zone
+#Set-TimeZone -Id $WindowsTimeZone
+
+Write-Host "[+] Time zone has been updated to - $WindowsTimeZone" -ForegroundColor Green 
